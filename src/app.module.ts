9c249395224fa5eaa1b465ee4e8d5b6ccbe00b00
ryu820 <rhju820@naver.com>
2023-06-20 02:21:30 +0900

    [DAY4]왜 환경변수 설정하면 db 연결이 안될까아아아아

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,30 @@ import { BoardsModule } from './boards/boards.module';
 // import { typeORMConfig } from './configs/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Board } from './boards/board.entity';
+import { config } from 'dotenv';
+config();
 
 @Module({
   //모듈에 typeormconfig 추가해주기!
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('USER_HOST'),
-        port: configService.get<number>('USER_PORT'),
-        username: configService.get<string>('USER_USERNAME'),
+        port: +configService.get<number>('USER_PORT'),
+        username: configService.get<string>('USER_NAME'),
         password: configService.get<string>('USER_PASSWORD'),
-        database: 'board-app',
-        entities: [__dirname + '../**/*.entity.{js,ts}'],
-        synchronize: false,
+        database: configService.get<string>('USER_DB'),
+        entities: [Board],
+        synchronize: true,
       }),
     }),
     BoardsModule,
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,14 +4,13 @@ import { config } from 'dotenv';
 
 config();
 const configService = new ConfigService();
-
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: configService.get<string>('USER_HOST'),
-  port: configService.get<number>('USER_PORT'),
-  username: configService.get<string>('USER_USERNAME'),
+  port: +configService.get<number>('USER_PORT'),
+  username: configService.get<string>('USER_NAME'),
   password: configService.get<string>('USER_PASSWORD'),
-  database: 'board-app',
+  database: configService.get<string>('USER_DB'),
   entities: [__dirname + '../**/*.entity.{js,ts}'],
   synchronize: true,
 };
